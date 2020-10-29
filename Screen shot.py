8141a7b971d618dd0dfe94006cb0fcfdfbe23c60
M.Prasanna <60608202+prasanna815@users.noreply.github.com>
2020-10-29 18:43:08 +0530

    Update Screen shot.py

--- Screen shot.py
+++ Screen shot.py
@@ -1,13 +1,15 @@
+from tkinter import *
 import pyautogui
-import tkinter as pras
-root=pras.Tk()
-canvas=pras.Canvas(root,width=300,height=300)
-canvas.pack()
+import time
 def fun():
+    root.withdraw()
+    time.sleep(1)
     run=pyautogui.screenshot()
     run.save('C:/Users/91812/Desktop/scrsht.png')
-btn=pras.Button(text="Takescreenshot",command=fun(),bg="red",fg="white",font=10)
-canvas.create_window(150,150,window=btn)
+root=Tk()
+root.config(bg='red')
+button=Button(root,text="TakeScreenShot",bg="pink",command=fun)
+button.pack()
 root.mainloop()
-print("Your screen shot is saved")
+
     
